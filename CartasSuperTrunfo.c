ae88ecf1141b196854a211c48c55a649147c4a3b
Robson Andrade <113925056+RobsonAndrade1@users.noreply.github.com>
2025-02-18 01:19:03 +0000

    Finalizando o Desafio Nível Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,34 +1,42 @@
-#include <stdio.h>
-
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+
+#include <stdio.h>
 
 int main() {
 
-    char Pais [50];
-    char Estado [50];
-    char Codigo [50];
-    int Populacao;
-    float Area;
-    float Pib;
-    int Pontos[20];
+// Variáveis para armazenar as informações da Carta 1
+
+    char Pais [20], Estado [10], Codigo [10], Cidade[50];
+    float Area, Pib;
+    int  Populacao, Pontos;
+
+// Variáveis para armazenar as informações da Carta 2
+
+     char Pais2[20], Estado2[10], Codigo2[10], Cidade2[50];
+     float Area2, Pib2;
+     int Populacao2, Pontos2;
+
+// Cadastro da Carta 1
 
     printf("*** Países ***\n");
 
-    printf("Digite o País \n");
-    scanf("%s", &Pais);
+    printf("*** Carta 1 ***\n");
 
-    printf("Digite o Estado \n");
+    printf("Digite o País \n");
+    scanf("%s", &Pais); 
+
+    printf("Digite o Código do Estado \n");
     scanf("%s", &Estado);
 
     printf("Digite o Códio da Carta \n");
     scanf("%s", &Codigo);
 
+    printf("Digite o Nome da Cidade \n");
+    scanf(" %[^\n]", &Cidade); // Alteração para aceitar espaços
+
     printf("Digite o Numero De Habitantes \n");
-    scanf("%s", &Populacao);
+    scanf("%d", &Populacao);
 
     printf("Digite a Àrea \n");
     scanf("%f", &Area);
@@ -39,21 +47,61 @@ int main() {
     printf("Digite Os Pontos Turisticos \n");
     scanf("%d", &Pontos);
 
-    printf("País: %s \n" , Pais);
+    printf("\n"); // Pula Uma Linha entre as cartas
+
+// Cadastro da Carta 2    
+
+    printf("*** Carta 2 ***\n");
+
+    printf("Digite o País \n");
+    scanf("%s", &Pais2); 
+
+    printf("Digite o Código do Estado \n");
+    scanf("%s", &Estado2);
+
+    printf("Digite o Códio da Carta \n");
+    scanf("%s", &Codigo2);
+
+    printf("Digite o Nome da Cidade \n");
+    scanf(" %[^\n]", &Cidade2); // Alteração para aceitar espaços
+
+    printf("Digite o Numero De Habitantes \n");
+    scanf("%d", &Populacao2);
+
+    printf("Digite a Àrea \n");
+    scanf("%f", &Area2);
+
+    printf("Digite o PIB \n");
+    scanf("%f", &Pib2);
+
+    printf("Digite Os Pontos Turisticos \n");
+    scanf("%d", &Pontos2);
+
+// Exibe as informações da Carta 1    
+
+    printf("\nCarta 1:\n");
     printf("Estado: %s \n", Estado);
     printf("Código: %s \n", Codigo);
+    printf("Nome da Cidade: %s \n", Cidade);
     printf("População: %d \n", Populacao);
+    printf("Àrea: %f km² \n", Area);
+    printf("PIB: %f bilhões de reais \n", Pib);
+    printf("Número de Pontos Turísticos %d \n", Pontos);
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("\n"); // Pula Uma Linha
+
+// Exibe as informações da Carta 2 
+ 
+    printf("\nCarta 2:\n");
+    printf("Estado: %s \n", Estado2);
+    printf("Código: %s \n", Codigo2);
+    printf("Nome da Cidade: %s \n", Cidade2);
+    printf("População: %d \n", Populacao2);
+    printf("Àrea: %f km² \n", Area2);
+    printf("PIB: %f bilhões de reais \n", Pib2);
+    printf("Número de Pontos Turísticos %d \n", Pontos2);
+    printf("\n"); // Pula Uma Linha
+
 
     return 0;
 }
